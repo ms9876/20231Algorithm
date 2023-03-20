0d470f54f8a5903c4935c1809fbb5169131b55aa
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-03-20 20:05:08 +0900

    계단 오르기4-1 2835

--- 2023Algorithm/20230320.cpp
+++ 2023Algorithm/20230320.cpp
@@ -103,4 +103,33 @@ int main() {
 }
 #pragma endregion
 
+#pragma region 계단 오르기4-1 2835 
+int n, k;
+
+int solve(int oo, int prev, int y) {
+    // 종료 조건
+    if (oo > n || y >= k)
+        return 0;
+    if (oo == n)
+        return 1;
+
+    if (prev == 0) {
+        return solve(oo + 1, 1, y + 1) + solve(oo + 2, 2, y + 1) + solve(oo + 3, 3, y + 1);
+    }
+    else if (prev == 1) {
+        return solve(oo + 2, 2, y + 1) + solve(oo + 3, 3, y + 1);
+    }
+    else if (prev == 2) {
+        return solve(oo + 1, 1, y + 1) + solve(oo + 3, 3, y + 1);
+    }
+    else if (prev == 3) {
+        return solve(oo + 1, 1, y + 1) + solve(oo + 2, 2, y + 1);
+    }
+}
+
+int main() {
+    cin >> n >> k;
+    cout << solve(0, 0, 0);
+}
+#pragma endregion
 
