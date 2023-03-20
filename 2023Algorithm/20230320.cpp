c5380f5a6e600e545b1dd7788447a20ed9baf0ab
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-03-20 19:26:00 +0900

    계단 오르기3-1 2834

--- 2023Algorithm/20230320.cpp
+++ 2023Algorithm/20230320.cpp
@@ -22,7 +22,7 @@ using namespace std;
 #pragma endregion
 
 //상태 정의
-#pragma region 계단 오르기 2832
+#pragma region 계단 오르기1-1 2832
 int n, k, cnt;
 
 // 어떤 상태를 정의할거냐
@@ -49,3 +49,58 @@ int main() {
     cout << cnt;
 }
 #pragma endregion
+
+#pragma region 계단 오르기2-1 2833
+int n, k, cnt;
+
+void s(int x, int y) {
+    if (x > n || y >= k) {
+        return;
+    }
+
+    if (x == n) {
+        cnt++;
+        return;
+    }
+
+    s(x + 1, y + 1);
+    s(x + 2, y + 1);
+}
+
+int main() {
+    cin >> n >> k;
+    s(0, 0);
+    cout << cnt;
+}
+#pragma endregion
+
+#pragma region 계단 오르기3-1 2834
+
+int n, k;
+int memo[40][40][40];
+int chk[40][40][40];
+// oo의 위치 xx의 위치, k(제한사항)
+int solve(int oo, int xx, int y) {
+    // 종료 조건
+    if (oo > n || xx > n || y >= k) 
+        return 0;
+    if (oo == n && xx == n) {
+        return 1;
+    }
+    if (chk[oo][xx][y] == 1) {
+        return memo[oo][xx][y];
+    }
+    chk[oo][xx][y] = 1;
+    return memo[oo][xx][y] = solve(oo + 1, xx + 1, y + 1) + 
+    solve(oo + 2, xx + 1, y + 1) +
+    solve(oo + 1, xx + 2, y + 1) +
+    solve(oo + 2, xx + 2, y + 1);
+}
+
+int main() {
+    cin >> n >> k;
+    cout << solve(0, 0, 0); 
+}
+#pragma endregion
+
+
