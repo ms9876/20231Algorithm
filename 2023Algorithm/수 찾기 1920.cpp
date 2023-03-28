4a874178f1f785c92cc38dd58abfd1e76f4d3fa0
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-03-28 19:10:00 +0900

    랜선 자르기 1654

--- 2023Algorithm/수 찾기 1920.cpp
+++ 2023Algorithm/수 찾기 1920.cpp
@@ -1,39 +1,45 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
-int n, vn, m , vm;
-vector <int> v;
+int n, vn;
+int m, vm;
+vector<int> v;
 
 int bisearch(int x) {
 	int low = 0;
 	int high = n - 1;
-	while (low < high)
-	{
+
+	while (low <= high) {
 		int mid = (low + high) / 2;
 		if (v[mid] == x) return 1;
 		else if (v[mid] > x) high = mid - 1;
 		else if (v[mid] < x) low = mid + 1;
 	}
+	return 0;
 }
+
 int main() {
 	ios_base::sync_with_stdio(false);
-	cin.tie(nullptr);
-	cout.tie(nullptr);
-	cin >> n;
+	cin.tie(0);
+	cout.tie(0);
 
-	for (int i = 0; i < n; i++) {
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
 		cin >> vn;
 		v.push_back(vn);
 	}
-	sort(v.begin(), v.end());
-	cin >> m;
-	for (int i = 0; i < m; i++) {
-		cin >> vm;
-		cout << bisearch(vm) << "\n";
-	}
 
+	sort(v.begin(), v.end());
+
+	cin >> m;
+	for (int i = 0; i < m; i++)
+	{
+		cin >> vm;
+		cout << bisearch(vm) << '\n';
+	}
 #pragma region ³» ÄÚµå
 	//int x, n, m;
 	//int arr[100000];
