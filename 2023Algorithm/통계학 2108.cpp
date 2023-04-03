656319c54263f0cb6c338fd9c6b746438dbe12b2
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-04-04 00:06:53 +0900

    다리 놓기 1010

---
+++ 2023Algorithm/통계학 2108.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+vector <int> v;
+int n;
+int main() {
+	// »ê¼ú Æò±Õ :
+	
+	// Áß¾Ó°ª : 
+	// ÃÖºó°ª : 
+	// ¹üÀ§ :
+	cout << (v.end() - 1) - v.begin();
+}
