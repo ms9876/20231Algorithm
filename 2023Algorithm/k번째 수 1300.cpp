4f5ba72dc550b035ae041fb010d6d902f74fb47a
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-04-04 20:28:51 +0900

    숫자 카드2 10816

--- 2023Algorithm/k번째 수 1300.cpp
+++ 2023Algorithm/k번째 수 1300.cpp
@@ -1,12 +1,18 @@
 #include <iostream>
 #include <algorithm>
+#include <vector>
 using namespace std;
 
 int n, k;
 int cnt, result;
+int arr[] = { 1,2,2,3,3,5,5,5,6 };
+vector <int> v = { 1,1,2,2,3,3,4,4,5 };
 
 int main()
 {
+    //cout << lower_bound(arr, arr + 9, 4) - arr << endl; // 5Ãâ·Â ( ÁÖ¼Ò°¡ ³ª¿È )
+    //int test = *lower_bound(arr, arr + 9, 4); ÀÌ°Ô Âð 5
+
     cin >> n >> k;
 
     int low = 1;
