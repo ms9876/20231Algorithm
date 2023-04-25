eb8e5c21ce2d3c92c64f1ab4a9f6b5cf76fd3b9b
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-04-25 18:01:54 +0900

    대칭 차집합 1269

---
+++ 2023Algorithm/대칭 차집합 1269.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <set>  
+
+using namespace std;
+
+int main() {
+	set<int> s;
+	int n, m, a;
+	cin >> n >> m;
+
+	for (int i = 0; i < n; i++) {
+		cin >> a;
+
+		s.insert(a);
+	}
+	for (int i = 0; i < m; i++) {
+		cin >> a;
+
+		if (s.count(a)) {
+			s.erase(a);
+		}
+		else {
+			s.insert(a);
+		}
+	}
+
+	cout << s.size();
+
+	return 0;
+}
