6c7af11a3343790a670848e92356eaab976b409e
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-04-25 19:25:04 +0900

    패션왕 신해빈 9375

--- 2023Algorithm/대칭 차집합 1269.cpp
+++ 2023Algorithm/대칭 차집합 1269.cpp
@@ -17,15 +17,36 @@ int main() {
 	for (int i = 0; i < m; i++) {
 		cin >> a;
 
-		if (s.count(a)) {
+		if (s.count(a)) 
 			s.erase(a);
-		}
-		else {
+		else 
 			s.insert(a);
-		}
 	}
 
 	cout << s.size();
-
-	return 0;
 }
+
+
+#pragma region Map 풀이
+//#include <iostream>
+//#include <map>
+//using namespace std;
+//
+//map<int, bool> m;   // key로 정수형을, value로 bool형 값을 가지는 map 생성
+//int N, M;
+//int num;
+//
+//int main(void)
+//{
+//	cin >> N >> M;  // N과 M을 입력받음
+//	for (int i = 0; i < N + M; i++)  // N+M개의 수를 입력받음
+//	{
+//		cin >> num;
+//		if (m[num] == true)  // 입력받은 수가 이미 존재하는 경우
+//			m.erase(num);   // 해당 수를 map에서 제거
+//		else                // 입력받은 수가 존재하지 않는 경우
+//			m[num] = true;  // 해당 수를 map에 추가
+//	}
+//	cout << m.size();   // 남은 map의 크기를 출력
+//}
+#pragma endregion
---
+++ 2023Algorithm/패션왕 신해빈 9375.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+
+//#pragma region 내 코드
+//int testcase, n, result;   // testcase: 테스트 케이스 수, n: 각 테스트 케이스의 태그 수, result: 각 테스트 케이스의 결과값
+//string name, tag;          // name: 태그의 이름, tag: 태그의 종류
+//map<string, int> m;        // 맵 자료구조를 이용하여 각 태그가 나타난 횟수를 기록
+//
+//int main() {
+//	cin >> testcase;
+//
+//	for (int i = 0; i < testcase; i++) {
+//		cin >> n;
+//
+//		for (int j = 0; j < n; j++) {
+//			cin >> name >> tag;
+//
+//			m[tag] = j;
+//
+//		}
+//	}
+//
+//}
+//#pragma endregion
+
+#pragma region 선생님 코드
+int testcase, n, result;
+string name, tag;
+map<string, int> m;
+int main() {
+    cin >> testcase;
+    for (int i = 0; i < testcase; i++)
+    {
+        result = 1;
+        cin >> n;
+        for (int i = 0; i < n; i++)
+        {
+            cin >> name >> tag;
+            m[tag]++;
+        }
+        for (const auto& iter : m)
+        {
+            result *= (iter.second +1);
+        }
+        cout << result - 1 << '\n';
+        m.clear();
+    }
+}
+#pragma endregion
+
