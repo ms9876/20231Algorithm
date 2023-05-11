fcd9ed583b1cf98028b17bed73b8b2172ad284d7
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-05-11 16:14:39 +0900

    카드2 2164

--- 2023Algorithm/큐2 18258.cpp
+++ 2023Algorithm/큐2 18258.cpp
@@ -138,3 +138,54 @@ int main() {
 //    }
 //}
 #pragma endregion
+
+#pragma region µÇ´Â º¤ÅÍ
+//int main()
+//{
+//    ios_base::sync_with_stdio(false);
+//    cin.tie(NULL);
+//    cout.tie(NULL);
+//    vector<int>myqueue;
+//    int n;
+//    string str;
+//    cin >> n;
+//    cin.ignore();
+//    while (n--)
+//    {
+//        getline(cin, str);
+//        if (str.substr(0, 4) == "push")
+//        {
+//            myqueue.push_back(stoi(str.substr(5)));
+//        }
+//        else if (str == "front")
+//        {
+//            if (myqueue.empty()) cout << -1 << '\n';
+//            else    cout << myqueue.front() << '\n';
+//
+//        }
+//        else if (str == "back")
+//        {
+//            if (myqueue.empty()) cout << -1 << '\n';
+//            else    cout << myqueue.back() << '\n';
+//        }
+//        else if (str == "size")
+//        {
+//            cout << myqueue.size() << '\n';
+//        }
+//        else if (str == "empty")
+//        {
+//            cout << myqueue.empty() << '\n';
+//        }
+//        else if (str == "pop")
+//        {
+//            if (myqueue.empty()) cout << -1 << '\n';
+//            else
+//            {
+//                myqueue.erase(myqueue.begin());
+//            }
+//        }
+//
+//    }
+//}
+#pragma endregion
+
