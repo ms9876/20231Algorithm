0ba27ca28cc42e057fb91d7bd820695f6f3d819f
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-05-18 16:19:47 +0900

    요세푸스 문제 1158 (큐)

--- 2023Algorithm/직사각형에서 탈출 1085.cpp
+++ 2023Algorithm/직사각형에서 탈출 1085.cpp
@@ -3,13 +3,13 @@
 
 using namespace std;
 
-int x, y, w, h;
+int x, v, w, h;
 int temp1, temp2;
 
 int main() {
-    cin >> x >> y >> w >> h;
+    cin >> x >> v >> w >> h;
 
-    temp1 = min(x, y);
-    temp2 = min(w - x, h - y);
+    temp1 = min(x, v);
+    temp2 = min(w - x, h - v);
     cout << min(temp1, temp2);
 }
