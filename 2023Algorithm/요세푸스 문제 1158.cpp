41417592c22f111dc24b00d8cc7a04dc2c931f77
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-05-19 22:41:39 +0900

    쉽게 푸는 문제 1292

--- 2023Algorithm/요세푸스 문제 1158.cpp
+++ 2023Algorithm/요세푸스 문제 1158.cpp
@@ -4,32 +4,65 @@
 using namespace std;
 
 #pragma region 큐
-int n, k;
-queue < int> q;
+//int n, k;
+//queue < int> q;
+//
+//int main() {
+//	cin >> n >> k;
+//	for (int i = 0; i < n; i++)
+//	{
+//		q.push(i);
+//	}
+//	cout << "<";	
+//	while (!q.empty())
+//	{
+//		for (int i = 0; i < k; i++)
+//		{
+//			if (i != k - 1)
+//				q.push(q.front());
+//			else if (q.size() > 1)
+//				cout << q.front() + 1 << ", ";
+//			else
+//				cout << q.front() + 1;
+//			q.pop();
+//		}
+//	}
+//	cout << ">";
+//}
+#pragma endregion
 
+#pragma region 연결리스트
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+
+	int n, k;
+
 	cin >> n >> k;
+
+	list<int> circleList;
 	for (int i = 0; i < n; i++)
 	{
-		q.push(i);
+		circleList.push_back(i);
 	}
-	cout << "<";	
-	while (!q.empty())
+	list<int>::iterator target = circleList.begin();
+
+	cout << "<";
+	while (n > 0)
 	{
 		for (int i = 0; i < k; i++)
 		{
-			if (i != k - 1)
-				q.push(q.front());
-			else if (q.size() > 1)
-				cout << q.front() + 1 << ", ";
-			else
-				cout << q.front() + 1;
-			q.pop();
+			target++;
 		}
+		if (n == 1) {
+			cout << *target;
+			break;
+		}	
+		cout << *target << ", ";
+		n--;	
+		target = circleList.erase(target);
 	}
 	cout << ">";
 }
-
 #pragma endregion
-
-
