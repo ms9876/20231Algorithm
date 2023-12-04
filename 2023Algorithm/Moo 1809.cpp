266d553dd8c4dd09f01463c7131ab3ef9994c15e
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-12-04 23:23:54 +0900

    모음의 개수 1264

---
+++ 2023Algorithm/Moo 1809.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	ios_base::sync_with_stdio(false);
+//	cin.tie(NULL); cout.tie(NULL);
+//
+//	cout << "(___)" << '\n';
+//	cout << "(o o)____/" << '\n';
+//	cout << " @@      \\" << '\n';
+//	cout << "  \\ ____,/" << '\n';
+//	cout << "  //   //" << '\n';
+//	cout << " ^^   ^^" << '\n';
+// }
