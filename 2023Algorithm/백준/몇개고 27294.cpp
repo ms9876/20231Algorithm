5c1552a989711671cb0fbaf3f0ce15bb8a404b94
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-01-06 11:10:42 +0900

    몇개고 27294

---
+++ 2023Algorithm/백준/몇개고 27294.cpp
@@ -0,0 +1,16 @@
+// https://www.acmicpc.net/problem/27294
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int t, s;
+
+	cin >> t >> s;
+
+	if (t >= 12 && t <= 16 && s == 0) 
+		cout << 320;
+	else
+		cout << 280;
+}
