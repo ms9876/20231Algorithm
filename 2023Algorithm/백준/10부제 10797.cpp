ca956065dea74f1103ba847879a8e7a0ea8f740f
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-01-08 00:02:48 +0900

    10부제 10797

--- 2023Algorithm/백준/10부제 10797.cpp
+++ 2023Algorithm/백준/10부제 10797.cpp
@@ -10,6 +10,14 @@ int main() {
 
 	for (int i = 0; i < 5; i++)
 	{
-
+		cin >> arr[i]; // 입력 받아주기
 	}
+
+	for (int i = 0; i < 5; i++)
+	{
+		if (n == arr[i]) // n에서 입력 받은 값과 i번의 값이 같다면
+			cnt++; // 카운트 올려주기
+	}
+
+	cout << cnt;
 }
