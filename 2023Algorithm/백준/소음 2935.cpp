8e6fff308c86e824739cd85a120bbf5cf65bd7af
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-02-12 15:01:29 +0900

    세 수 2985

---
+++ 2023Algorithm/백준/소음 2935.cpp
@@ -0,0 +1,15 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main() {
+//	int n, m;
+//	char c;
+//
+//	cin >> n >> c >> m;
+//
+//	if (c == '*')
+//		cout << n * m;
+//	else if (c == '+')
+//		cout << n + m;
+//}
