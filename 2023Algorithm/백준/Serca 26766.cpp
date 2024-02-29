fb143870fb89f5cc0bb9f18245621cf54022ffee
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-02-29 10:45:39 +0900

    Serca 26766

---
+++ 2023Algorithm/백준/Serca 26766.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+	int n;
+
+	cin >> n;
+	
+	while (n--) {
+		cout << " @@@   @@@ " << '\n';
+		cout << "@   @ @   @" << '\n';
+		cout << "@    @    @" << '\n';
+		cout << "@         @" << '\n';
+		cout << " @       @ " << '\n';
+		cout << "  @     @  " << '\n';
+		cout << "   @   @   " << '\n';
+		cout << "    @ @    " << '\n';
+		cout << "     @     " << '\n';
+	}
+}
