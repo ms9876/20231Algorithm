c0457cb888bf79ac4a9f0b040cd0d15a23da5314
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-03-24 23:08:50 +0900

    특별한 날 10768

--- 2023Algorithm/백준/Adding Trouble 31654.cpp
+++ 2023Algorithm/백준/Adding Trouble 31654.cpp
@@ -11,3 +11,4 @@ int main()
     else
         cout << "wrong!";
 }
+
---
+++ 2023Algorithm/백준/특별한 날 10768.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n, m;
+
+    cin >> n >> m;
+
+    if (n == 2) {
+        if (m == 18) {
+            cout << "Special";
+        }
+        else if (m > 18) {
+            cout << "After";
+        }
+        else cout << "Before";
+    }
+    else if (n > 2)
+        cout << "After";
+    else 
+        cout << "Before";
+}
