208f00697d8f82ccd7c8a2d13caa84a36d6805ed
Chae min seo <98958229+ms9876@users.noreply.github.com>
2024-05-28 22:13:12 +0900

    치킨 쿠폰 1673

---
+++ 2023Algorithm/백준/치킨 쿠폰 1673.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, k;
+    int meat, stamp;
+    while (cin >> n >> k)
+    {
+        meat = 0;
+        stamp = 0;
+
+        while (true)
+        {
+            meat += n;
+            stamp += n;
+
+            n = 0;
+            n += stamp / k;
+
+            stamp = stamp % k;
+
+            if (n == 0)
+                break;
+        }
+
+        cout << meat << endl;
+    }
+}
